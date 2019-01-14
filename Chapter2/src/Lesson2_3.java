05123f3b2bb59e90f02da4355606bba68dbbbb1d
Colloid <c011319546@edu.teu.ac.jp>
2019-01-14 23:06:05 +0900

    コンパイル済

---
+++ Chapter2/src/Lesson2_3.java
@@ -0,0 +1,18 @@
+public class Lesson2_3 {
+    public static void main(String args[]){
+        Sysout("ようこそ占いの館へ\nあなたの名前を教えてください");
+        String name = new java.util.Scanner(System.in).nextLine();
+        Sysout("あなたの年齢を入力してください");
+        String ageString = new java.util.Scanner(System.in).nextLine();
+        int age = Integer.parseInt(ageString);
+        int fortune = new java.util.Random().nextInt(4);
+        fortune++;
+        Sysout("画面に占いの結果が出ました!");
+        Sysout(age+"歳の"+name+"さん,あなたの運気番号は"+fortune+"です");
+        Sysout("1:大吉　2:中吉　3:吉　4:凶");
+    }
+
+    private static void Sysout(String message){
+        System.out.println(message);
+    }
+}
